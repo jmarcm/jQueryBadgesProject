988e0806f39cdf6c993050a5c98dbe0681ff3dc2
Jean-Marc <montout.jeanmarc@gmail.com>
2016-12-26 04:52:33 -0400

    Moving to function showCourses
    still using $.each

--- assets/main.js
+++ assets/main.js
@@ -4,28 +4,32 @@ $(function() {
       url: "https://www.codeschool.com/users/jmarcm.json",
       dataType: "jsonp",
       success: function(response) {
-          var coursesCompleted = response.courses.completed;
-          console.log(coursesCompleted);
+          showCourses(response.courses.completed);
 
-          $.each(coursesCompleted, function(i, courseCompleted) {
-              var $div = $("<div>", {class: "course"}).appendTo("#badges");
-
-              $("<h3>", {
-                  html: courseCompleted.title
-              }).appendTo($div);
-
-              $("<img>", {
-                  src: courseCompleted.badge
-              }).appendTo($div);
-
-              $("<a>", {
-                  href: courseCompleted.url,
-                  target: "_blank",
-                  class: "btn btn-primary",
-                  text: "See course"
-              }).appendTo($div);
-          })
       }
-  })
+  });
+
+
+  function showCourses(coursesCompleted) {
+
+      $.each(coursesCompleted, function(i, courseCompleted) {
+          var $div = $("<div>", {class: "course"}).appendTo("#badges");
+
+          $("<h3>", {
+              html: courseCompleted.title
+          }).appendTo($div);
+
+          $("<img>", {
+              src: courseCompleted.badge
+          }).appendTo($div);
+
+          $("<a>", {
+              href: courseCompleted.url,
+              target: "_blank",
+              class: "btn btn-primary",
+              text: "See course"
+          }).appendTo($div);
+      });
+  }
 
 });
